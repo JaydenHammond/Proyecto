58a0e444f91d19cf4860ed1150549a7b50aed628
JaydenHammond <143233194+JaydenHammond@users.noreply.github.com>
2023-10-29 02:31:06 +0000

    Doom

--- include/Controlador.hpp
+++ include/Controlador.hpp
@@ -9,23 +9,47 @@ private:
 public:
     Controlador(/* args */) {}
     ~Controlador() {}
-    void Actualizar(){
-        // if(getch() == 'q')
-        // {
-        //     ejecucion = false;
-        // }
-        // if(getch() == 'd'){
-        //     d1.AvanzarX(1);
-        // }
-        // if(getch() == 'a'){
-        //     d1.RetrocederX(1);
-        // }
-        // if(getch() == KEY_RIGHT){
-        //     d2.AvanzarX(1);
-        // }
-        // if(getch() == KEY_LEFT){
-        //     d2.RetrocederX(1);
-        // }
-
+    void Actualizar()
+    {
+        if (getch() == 'q')
+        {
+            ejecucion = false;
+        }
+        if (getch() == 'd')
+        {
+            d1.AvanzarX(1);
+        }
+        if (getch() == 'a')
+        {
+            d1.RetrocederX(1);
+        }
+        if (getch() == 'w')
+        {
+            d1.AvanzarY(1);
+        }
+        if (getch() == 's')
+        {
+            d1.RetrocederY(1);
+        }
+        if (getch() == 'f')
+        {
+            d1.disparar(1);
+        }
+        if (getch() == KEY_RIGHT)
+        {
+            d2.AvanzarX(1);
+        }
+        if (getch() == KEY_LEFT)
+        {
+            d2.RetrocederX(1);
+        }
+        if (getch() == KEY_UP)
+        {
+            d2.AvanzarX(1);
+        }
+        if (getch() == KEY_DOWN)
+        {
+            d2.RetrocederX(1);
+        }
     }
 };
---
+++ include/Imp.hpp
@@ -0,0 +1,9 @@
+#pragma once
+class Imp
+{
+private:
+    /* args */
+public:
+    Imp(/* args */) {}
+    ~Imp() {}
+};
